09b91e711e594a23bda6d9046e9f6b51c67c0fae
Лейкин Андрей Романович <Leykin.AR@kamaz.org>
2017-09-18 13:51:55 +0300

    diagram

--- source/js/common/diagram.js
+++ source/js/common/diagram.js
@@ -1,25 +1,46 @@
 // Функция для диаграм на странице сравнения
 
 (function(){
-	var block = $('.diagram__img');
+	var block = $('.diagram__img'),
+		counter = $('.diagram__price');
+
 
 	$(window).scroll(function() {
 		
 		var scrollTop = $(this).scrollTop();
-		if (checkDistance(scrollTop)) {
-			for (i=0; i<block.length;i++) {
-				var height = block.eq(i).attr('data-height');
-				block.eq(i).animate({height:0, height}, 1000);
+
+		for (i=0; i<block.length;i++) {
+			var blockEq = block.eq(i),
+				counterEq = counter.eq(i),
+				parent = counterEq.closest('.diagram__item'),
+				height = blockEq.attr('data-height'),
+				dataCounter = counter.eq(i).attr('data-counter');
+
+
+			if (checkDistance(scrollTop, blockEq)) {
+				blockEq.animate({height:0, height}, 1000);
+				counterEq.animate({num: dataCounter - 3}, {
+					duration: 1000,
+					step: function (num) {
+						if (this.closest('.diagram__item--price')) {
+							this.innerHTML = (num + 3).toFixed(0) + ' тыс';
+						} else if(this.closest('.diagram__item--cost') || this.closest('.diagram__item--service') || this.closest('.diagram__item--month')){
+							this.innerHTML = (num + 3).toFixed(0) + ' р';
+						} else {
+							this.innerHTML = (num + 3).toFixed(0);
+						}
+					}
+				});
 			}
 		}
 	});
 
 
-	var checkDistance = function(scrollTop) {
+	var checkDistance = function(scrollTop, block) {
 		var offset = block.offset().top,
 			windowMargin = Math.ceil($(window).height() / 3),
-			topBorder = offset - scrollTop - windowMargin - 400,
-			bottomEdge = block.outerHeight(true) + offset + 150,
+			topBorder = offset - scrollTop - windowMargin - 800,
+			bottomEdge = block.outerHeight(true) + offset + 100,
 			bottomBorder = scrollTop + windowMargin - bottomEdge;
 
 		return topBorder <= 0 && bottomBorder <= 0
