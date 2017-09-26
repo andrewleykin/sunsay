02cea056a31d11bce1cc82e0b809742687d884cc
Andrew Leykin <katashi1328@mail.ru>
2017-09-26 16:13:15 +0300

    test page

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -4,11 +4,11 @@ module.exports = [
   './source/js/libs/wowjs.js',
   './source/js/libs/cocoen.js',
   './source/js/libs/svg4everybody.js',
+  './source/js/common/mask.js',
   './source/js/common/main-slider.js',
   './source/js/common/tech-slider.js',
   './source/js/common/diagram.js',
   './source/js/common/counter.js',
-  './source/js/common/mask.js',
   './source/js/common/basket-scroll.js',
   './source/js/common/card-show.js',
   './source/js/common/card-slider.js',
@@ -17,5 +17,6 @@ module.exports = [
   './source/js/common/coop-tabs.js',
   './source/js/common/menu.js',
   './source/js/common/footer.js',
-  './source/js/common/heating-menu.js'
+  './source/js/common/heating-menu.js',
+  './source/js/common/calculator.js'
 ];
